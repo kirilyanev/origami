3885b8d9bc16cda6acc1ba6428ab9918579824ba
Kiril Yanev <kl_yanev@abv.bg>
2023-08-17 18:50:27 +0300

    Create basic url routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Route, Link, NavLink, Redirect, Switch } from 'react-router-dom';
+import { Route, Link, NavLink, Redirect, Switch, Routes } from 'react-router-dom';
 
 import * as postService from './services/postService.js';
 // import { getAll } from './services/postService.js';
@@ -7,55 +7,61 @@ import * as postService from './services/postService.js';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Main from './components/Main';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
 import style from './App.module.css';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      posts: [],
-      selectedPost: null,
-    };
+        this.state = {
+            posts: [],
+            selectedPost: null,
+        };
 
-    this.onMenuItemClick = this.onMenuItemClick.bind(this);
-  }
-
-  componentDidMount() {
-    postService.getAll()
-      .then(posts => {
-        console.log('postove-' + posts);
-        this.setState({ posts });
-      });
-  }
-
-  onMenuItemClick(id) {
-    this.setState({ selectedPost: id });
-  }
-
-  getPosts() {
-    if (!this.state.selectedPost) {
-      return this.state.posts;
-    } else {
-      let result = [this.state.posts.find(x => x.id === this.state.selectedPost)];
-      return result;
+        this.onMenuItemClick = this.onMenuItemClick.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <div className={style.app}>
-        <Header />
+    componentDidMount() {
+        postService.getAll()
+            .then(posts => {
+                console.log('postove-' + posts);
+                this.setState({ posts });
+            });
+    }
 
-        <div className={style.container}>
-          <Menu onMenuItemClick={this.onMenuItemClick} />
+    onMenuItemClick(id) {
+        this.setState({ selectedPost: id });
+    }
 
-          <Main posts={this.getPosts()} />
-        </div>
-      </div>
-    );
-  }
+    getPosts() {
+        if (!this.state.selectedPost) {
+            return this.state.posts;
+        } else {
+            let result = [this.state.posts.find(x => x.id === this.state.selectedPost)];
+            return result;
+        }
+    }
+
+    render() {
+        return (
+            <div className={style.app}>
+                <Header />
+
+                <div className={style.container}>
+                    <Menu onMenuItemClick={this.onMenuItemClick} />
+
+                    <Routes>
+                        <Route path="/about" element={<About />} />
+                        <Route path='/contact-us' element={<ContactUs />} />
+                        <Route path='/' element={<Main posts={this.getPosts()} />} />
+                    </Routes>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
