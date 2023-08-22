1edf09ac99aca1c0a939b084cc68a54642002a9e
Kiril Yanev <kl_yanev@abv.bg>
2023-08-22 17:22:25 +0300

    Update Header links style when active

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,25 +1,25 @@
 import style from './Header.module.css';
 import NavigationItem from './NavigationItem';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className={style.navigation}>
             <ul>
                 <li className="listItem"><img src="white-origami.png" alt="white-origami" /></li>
-                <Link to="/"><NavigationItem>Home</NavigationItem></Link>
-                <Link to="/about"><NavigationItem>About</NavigationItem></Link>
-                <Link to="/contact-us"><NavigationItem>Contact us</NavigationItem></Link>
-                <Link to="/about/pesho"><NavigationItem>Pesho</NavigationItem></Link>
-                <Link to="/about/gosho"><NavigationItem>Going to 5</NavigationItem></Link>
-                <Link to="/about/stamat"><NavigationItem>Going to 6</NavigationItem></Link>
-                <Link to="/about/mariyka"><NavigationItem>Going to 7</NavigationItem></Link>
-                <Link to="/about/malinka"><NavigationItem>Going to 8</NavigationItem></Link>
-                <Link to="/about/tosho"><NavigationItem>Going to 9</NavigationItem></Link>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/" ><NavigationItem>Home</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about" end ><NavigationItem>About</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/contact-us" end ><NavigationItem>Contact us</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/pesho"><NavigationItem>Pesho</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/gosho"><NavigationItem>Going to 5</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/stamat"><NavigationItem>Going to 6</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/mariyka"><NavigationItem>Going to 7</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/malinka"><NavigationItem>Going to 8</NavigationItem></NavLink>
+                <NavLink style={(navData) => navData.isActive ? {backgroundColor: 'red'} : {backgroundColor: ''}} to="/about/tosho"><NavigationItem>Going to 9</NavigationItem></NavLink>
                 {/* <NavigationItem>Going to 10</NavigationItem> */}
                 {/* <NavigationItem>Going to 11</NavigationItem> */}
 
-                <Link to="/about">About</Link>
+                {/* <Link to="/about">About</Link> */}
             </ul>
         </nav>
     );
