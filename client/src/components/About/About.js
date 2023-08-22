ee7f5e20dba30f7add1edbe914d28b73e2a1625f
Kiril Yanev <kl_yanev@abv.bg>
2023-08-22 18:47:59 +0300

    Added conditional rendering in about.js

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,10 +1,17 @@
-import { useLocation, useNavigate, useParams} from 'react-router-dom';
+import { useLocation, useNavigate, useParams, Route, Routes } from 'react-router-dom';
 
 const About = () => {
     const params = useParams();
+    const pathname = useLocation().pathname;
+    
     return (
         <main className="main-container">
             <h1>About {params.name} Page</h1>
+
+            {pathname === '/about/pesho' ? 
+            <h2>Pesho is the best!!!</h2>
+            : ''
+            }
         </main>
     );
 };
